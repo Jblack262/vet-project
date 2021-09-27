dceb84386330482bad5929c80194c8f67eee59dd
Moleso587587 <71471833+Moleso587587@users.noreply.github.com>
2021-09-27 15:19:51 -0700

    Added banner to adopt page, changed form colors

--- src/pages/Adopt.js
+++ src/pages/Adopt.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Form from '../components/Form';
+import Banner from '../components/Banner';
 
 function Adopt() {
     return (
-        <div className='adopt'>
-            <h1>Adopt Page</h1>
-            <Form/>
-        </div>
+        <>
+            <Banner image="https://animalcareinfo.com/wp-content/uploads/2018/04/AdobeStock_129584968-1024x388.jpeg" title="Adopt a Pet"/>
+            <div className='adopt'>
+                <Form/>
+            </div>
+        </>
     )
 }
 
