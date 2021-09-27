b240e16ddab622dbe7d2582e3a59051b20816762
Moleso587587 <71471833+Moleso587587@users.noreply.github.com>
2021-09-27 14:50:46 -0700

    Created form that takes objects from a questions array, completely styled

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,32 +1,30 @@
 import React from 'react'
 
 function Form() {
-    /* const questions = [
-        'First & Last Name',
-        'Occupation',
-        'Address',
-        'Phone Number',
-        'Email Address',
-        ['How did you hear about our adoptable animals? Please list the specific person if applicable.', 1],
-        ['How many children live in your house?', 1],
-        ['What type of home do you live in?', 1],
-        ['If interested in a large dog, will you have adequate space to exercise them or walk them every day?', 1],
-        ['If under the age of 18 years, is your legal guardian aware and approving of the adoption of an animal from West-MEC?', ['Yes', 'No']],
-        [],
-        'Name',
-        'Phone Number',
-        'Email Address',
-        ['What pets do you currently have in your home (specific type and number)?', 1],
-        ['Are your pets sterilized and up to date on vaccinations?', ['Yes', 'No', "I don't have any other pets"]]
-
-    ]; */
+    const questions = [
+        {
+            id: 1,
+            title: 'Question?',
+            placeholder: 'placeholder'
+        },
+        {
+            id: 2,
+            title: 'Question?',
+            placeholder: 'placeholder'
+        }
+    ];
     return (
-        <>
+        <form className='adopt-form'>
             <h1>Adoption Application</h1>
-            <form>
-                <h1>Please provide contact info for legal guardian or adult responsible for residency of animal</h1>
-            </form>
-        </>
+            {questions.map(question=>{
+                return (
+                    <div key={question.id} className='question'>
+                            <h1>{question.title}</h1>
+                            <input type='text' placeholder={question.placeholder}></input>
+                    </div>
+                )
+            })}
+        </form>
     )
 }
 
