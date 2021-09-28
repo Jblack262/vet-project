2a30fd1cddabba2922ade5d91a39bccb652a2926
Moleso587587 <71471833+Moleso587587@users.noreply.github.com>
2021-09-28 14:18:13 -0700

    added form to pet page, restyled form to be more responsive

--- src/pages/PetPage.js
+++ src/pages/PetPage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Slideshow from '../components/Slideshow';
+import Form from '../components/Form';
 
 function PetPage({pet}) {
     const {name, species, gender, breed, age, campus, images} = pet;
     // console.log(images);
     return (
+        <>
         <div className="petContainer">
             <Slideshow images={images} name={name}/>
-
             <div className="petDescription">
                 <h1 className="name">{name}</h1>
                 <p className="description">Here would go a brief description of the pet, their personality maybe, details about them that are not included in the list below.</p>
@@ -20,6 +21,8 @@ function PetPage({pet}) {
                 </ul>
             </div>
         </div>
+        <Form/>
+        </>
     )
 }
 
