a173de5e8c8d6ca8c6a138f4c0efa6a0ed156317
Moleso587587 <71471833+Moleso587587@users.noreply.github.com>
2021-09-28 15:21:45 -0700

    added skelCard component and Load2, load 2 will eventually replace load

---
+++ src/components/SkelCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function SkelCard() {
+    return (
+        <div className="skelCard">
+            {/* <div className="imgContainer">
+                <img src={require("../images/dog.jpg").default} alt="cool dog" />
+            </div>
+            <div className="content">
+                <h2>{name}</h2>
+                <p>{gender} - {species} - {breed} - {age} - available at {campus}</p>
+                <div className="btnContainer">
+                    <Link onClick={() => selectPet(pet)} to="/pets">ADOPT ME</Link>
+                </div>
+            </div> */}
+            
+        </div>
+    )
+}
+
+export default SkelCard
