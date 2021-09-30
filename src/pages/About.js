37823b85f5b29a0edce3b8be6fc31ae43281a5e6
Amakris12 <amakri386@west-mec.org>
2021-09-30 14:02:56 -0700

    Fixed about page
    Fixed alignment on the two cards on the top of the page

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,7 +11,7 @@ function About() {
                 <title>About Page</title>
             </Helmet>
             <Banner image="https://animalcareinfo.com/wp-content/uploads/2018/04/AdobeStock_129584968-1024x388.jpeg" title="About Us" />
-            <div className="cardContainer">
+            <div className='cardInfo'>
                 <InfoCard title="About Us" content="The West-MEC Veterinary Sciences program has a partnership with Pinal County Animal Control to rescue animals. We are at the West mec northeast campus which is located at 1617 W Williams Dr Phoenix, AZ 85027. If you are interested in one of our pets, please schedule an appointment to visit with us as we are a public high school campus." />
                 <InfoCard title="What We Do" content="What we do here at the West-MEC Veterinary Sciences program is have Students work with a veterinarian to spay, neuter, and vaccinate the animals to prepare them for adoption. These four-legged companions are available for adoption and looking for their forever home." />
             </div>
